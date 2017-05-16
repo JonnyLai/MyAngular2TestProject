381a45724c74d6219f36669c171d500a5be17d3b
JonnyLai <m10573727@mail.nknu.edu.tw>
2017-05-16 11:31:27 +0800

    NextStep

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+
+  sitename = 'The Will Will Web - Jonny test';
+  siteimg = '/assets/images/logo.png';
+  constructor() { }
+
+  ngOnInit() {
+    //setTimeout(() => {
+    //  this.sitename = "Hello Jonny";
+    //}, 1000);
+  }
+
+  changeTitle($event) {
+    this.sitename = "Hello Jonny - Clicked";
+    console.log($event);
+  }
+
+}
